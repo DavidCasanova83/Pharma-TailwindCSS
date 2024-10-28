be1061280ff0949a1ca05d1dc1f5fe58db554455
David Casanova <casanova.83130@gmail.com>
2024-10-28 23:15:29 +0100

    start home page

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,14 +2,48 @@
 module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
-    extend: {
-      colors: {
-        'customBackground': '#FAFAFA',
-        'customBackgroundSecondary': '#FFFFFF',
-        'customPrimary': '#18181B',
+    container: {
+      paddding: {
+        DEFAULT: '15px',
       },
-  },
+    },
+    screens: {
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+    },
+    extend: {
+      lineHeight: {
+        '120': '120%',
+      },
+      colors: {
+        'primary': '#242a2b',
+        'secondary': '#808080',
+        accent: {
+          DEFAULT: '#1cbccf',
+          secondary: '#18abbc',
+          tertiary: '#90c6cd',
+        },
+        grey: '#e8f0f1',
+      },
+      fontFamily: {
+        primary: 'Poppins',
+      },
+      boxShadow: {
+        custom1: '0 2px 40px 0 rgba(8, 70, 78, 0.08)',
+        custom2: '0 0px 30px 0 rgba(8, 73, 81, 0.06)',
+      },
+      backgfroundImage: {
+        services: "url('../..assets/img/services/bg.svg')",
+        testimonials: "url('../..assets/img/testimonials/bg.svg')",
+        departments: "url('../..assets/img/departments/bg.svg')",
+        quoteLeft: "url('../..assets/img/testimonials/quote-left.svg')",
+        quoteRight: "url('../..assets/img/testimonials/quote-right.svg')",
+
+      },
+    },
   },
   plugins: [],
-}
+};
 
